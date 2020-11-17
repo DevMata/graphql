696f83f3d14726149e89afc76ff508a87a1f9931
Antonio Mata <hildeamp@gmail.com>
2020-11-16 23:12:23 -0600

    feat: add Post ids on comments array

--- src/index.js
+++ src/index.js
@@ -57,21 +57,25 @@ const comments = [
     id: 'a',
     text: 'The A text',
     author: '1',
+    post: '456',
   },
   {
     id: 'b',
     text: 'The B text',
     author: '2',
+    post: '123',
   },
   {
     id: 'c',
     text: 'The C text',
     author: '3',
+    post: '789',
   },
   {
     id: 'd',
     text: 'The D text',
     author: '2',
+    post: '147',
   },
 ];
 /*
