17e45740740bc7eb17b8e0b8a98e0314276cc283
Antonio Mata <hildeamp@gmail.com>
2020-11-17 22:40:24 -0600

    refactor: update createUser mutation

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ const typeDefs = `
   }
   
   type Mutation{
-    createUser(name:String!,email:String!,age:Int):User!
+    createUser(data:CreateUserInput):User!
     createPost(title:String!,body:String!,published:Boolean!,author:ID!):Post!
     createComment(text:String!,author:ID!,post:ID!):Comment!
   }
