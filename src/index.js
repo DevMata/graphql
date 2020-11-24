8474447a320041170c5738268173577036fc5fed
Antonio Mata <hildeamp@gmail.com>
2020-11-23 18:07:47 -0600

    refactor: move type defs to schema.graphql

--- src/index.js
+++ src/index.js
@@ -84,70 +84,6 @@ let comments = [
  * String, Boolean, Int, Float, ID
  * */
 
-// types definitions
-const typeDefs = `
-  type Query {
-    me: User!
-    post: Post!
-    users(query:String):[User!]!
-    posts(query:String):[Post!]!
-    comments(query:String):[Comment!]!
-  }
-  
-  type Mutation{
-    createUser(data:CreateUserInput):User!
-    deleteUser(id:ID!):User!
-    createPost(post:CreatePostInput):Post!
-    deletePost(id:ID!):Post!
-    createComment(comment:CreateCommentInput):Comment!
-    deleteComment(id:ID!):Comment!
-  }
-  
-  input CreateUserInput{
-    name:String!
-    email:String!
-    age:Int
-  }
-  
-  input CreatePostInput{
-    title:String!
-    body:String!
-    published:Boolean!
-    author:ID!
-  }
-  
-  input CreateCommentInput{
-    text:String!    
-    author:ID!
-    post:ID!
-  }
-  
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-    posts: [Post!]!
-    comments: [Comment!]!
-  }
-  
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-    author: User!
-    comments: [Comment!]!
-  }
-  
-  type Comment {
-    id: ID!,
-    text: String!
-    author: User!
-    post: Post!
-  }
-`;
-
 // Resolvers
 const resolvers = {
   Query: {
@@ -310,7 +246,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
