4918ed2a9671498639ff8545e9964871224f2502
Antonio Mata <hildeamp@gmail.com>
2020-12-17 22:27:12 -0600

    feat: add async methods for create and update posts

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -17,30 +17,46 @@ const log = (data) => console.log(JSON.stringify(data, null, 2));
   .then(log)
   .catch(log);*/
 
-/*prisma.mutation
-  .createPost(
+const createPostForUser = async (authorId, data) => {
+  await prisma.mutation.createPost(
     {
       data: {
-        title: 'My new post',
-        body: 'Body',
-        published: true,
-        author: { connect: { id: 'ckiguzkj100dh0747wnzw1cn8' } },
+        ...data,
+        author: { connect: { id: authorId } },
       },
     },
-    '{ id title body published author{ id name } }',
-  )
+    '{ id }',
+  );
+
+  return await prisma.query.user(
+    { where: { id: authorId } },
+    '{ id name email posts { id title body published } }',
+  );
+};
+
+const updatePostForUser = async (postId, data) => {
+  const post = await prisma.mutation.updatePost(
+    {
+      data,
+      where: { id: postId },
+    },
+    '{ author{ id } }',
+  );
+
+  return await prisma.query.user(
+    { where: { id: post.author.id } },
+    '{ id name email posts { id title body published } }',
+  );
+};
+
+/*createPostForUser('ckiguzkj100dh0747wnzw1cn8', {
+  title: 'Second post of the night',
+  body: 'A good body',
+  published: true,
+})
   .then(log)
   .catch(log);*/
 
-/*prisma.mutation
-  .updatePost(
-    {
-      data: {
-        published: true,
-      },
-      where: { id: 'ckitq7ihc00ak0847dhahte1k' },
-    },
-    '{id title body published author{ id name } }',
-  )
+/*updatePostForUser('ckitr2bs100sj0847jx1mzb0m', { published: false })
   .then(log)
   .catch(log);*/
